fdd4ef0b9c326a04774039cc44439782935a7e7f
Jake <46798353+jakeleaves@users.noreply.github.com>
2024-10-20 18:10:39 -0500

    deploy from main

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-/** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// next.config.mjs
 
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true, // Ensures URLs end with a slash for GitHub Pages compatibility
-};
-
-export default nextConfig;
+export default {
+    output: 'export',  // Use static export
+    trailingSlash: true,  // Optional but recommended for GitHub Pages
+  };
+  
