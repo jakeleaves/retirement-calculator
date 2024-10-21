d6caaca205b3aab0291d9d218636d290c029e3ff
Jake <46798353+jakeleaves@users.noreply.github.com>
2024-10-20 19:26:53 -0500

    bruhh

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/retirement-calculator',
-    assetPrefix: '/retirement-calculator/',
 };
 
 export default nextConfig;
